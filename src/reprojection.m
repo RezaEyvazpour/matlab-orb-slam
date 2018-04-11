3261beff8da7159b8a88485a6233d24ee6377c15
xinzhezhang <xinzhe@umich.edu>
2018-04-11 14:08:41 -0400

    added translation

--- src/reprojection.m
+++ src/reprojection.m
@@ -1,11 +1,13 @@
-function A = reprojection(y,y_gnd)
-%% Gives a best estimatin of transformation matrix A that minimizes the LS error
-% between y_gnd and A*y.
-% A = R*S, where S is the scaling in three directions and R is the Euler
-% rotation matrix
-% Requires the first point of y and y_gnd at (0,0) & num of points of y and
-% y_gnd the same
-% y, y_gnd = 3*n
+function A = reprojection(y,y_gnd,allowTranslation)
+%% A = reprojection(y,y_gnd,allowTranslation)
+% y, y_gnd = 3*n, allowTranslation = 0 or 1. Requires known DA between y
+% and y_gnd.
+% Gives a best estimatin of transformation matrix A that minimizes the LS
+% error between y_gnd and A*y.
+% If allowTranslation == 0, A = R*S is a 3 by 3 matrix of scaling and
+% rotation.
+% If allowTranslation == 1, A is a 4 by 4 matrix of scaling, rotation and
+% translation in three directions.
 y_gnd_avg = mean(y_gnd,2);
 y_avg = mean(y,2);
 s1 = norm(y_gnd_avg)/norm(y_avg);
@@ -13,12 +15,6 @@ S1 = s1*eye(3);
 y_est = S1*y;
 [R_1, ~] = icp_3d(y_est, y_gnd);
 y_est = R_1*y_est;
-% y_gnd_1 = y_gnd(:,2)-y_gnd(:,1);
-% y_gnd_2 = y_gnd(:,3)-y_gnd(:,1);
-% y_gnd_3 = y_gnd(:,4)-y_gnd(:,1);
-
-% y_1 = y(:,2)-y(:,1);
-% y_2 = y(:,3)-y(:,1);
 x0 = [1,0,0,0];
 options = optimoptions('lsqnonlin','Display','iter');
 options.Algorithm = 'levenberg-marquardt';
@@ -35,19 +31,27 @@ Rx = [1 0 0;0 cos(alpha) -sin(alpha);0 sin(alpha) cos(alpha)];
 R = Rz*Ry*Rx;
 S = diag([s1 s1 s1]);
 A = R*S*R_1*S1;
+y_est = A*y;
+if allowTranslation
+    x0 = [1,0,0,0,0,0,0];
+    func = @(x) distanceTrans(x,y_est,y_gnd);
+    [x,resnorm,residual,exitflag,output] = lsqnonlin(func,x0,[],[],options);
+    x
+    s1 = x(1);
+    alpha = x(2);
+    beta = x(3);
+    gamma = x(4);
+    xTrans = x(5);
+    yTrans = x(6);
+    zTrans = x(7);
+    T = [xTrans;yTrans;zTrans];
+    Rz = [cos(gamma) -sin(gamma) 0;sin(gamma) cos(gamma) 0;0 0 1];
+    Ry = [cos(beta) 0 sin(beta);0 1 0;-sin(beta) 0 cos(beta)];
+    Rx = [1 0 0;0 cos(alpha) -sin(alpha);0 sin(alpha) cos(alpha)];
+    R = Rz*Ry*Rx;
+    S = diag([s1 s1 s1]);
+    A = [S*R*A T;zeros(1,3) 1];
 end
-function F = initi(x,y_gnd_1,y_gnd_2,y_1,y_2)
-s1 = x(1);
-alpha = x(2);
-beta = x(3);
-gamma = x(4);
-Rz = [cos(gamma) -sin(gamma) 0;sin(gamma) cos(gamma) 0;0 0 1];
-Ry = [cos(beta) 0 sin(beta);0 1 0;-sin(beta) 0 cos(beta)];
-Rx = [1 0 0;0 cos(alpha) -sin(alpha);0 sin(alpha) cos(alpha)];
-R = Rz*Ry*Rx;
-S = diag([s1 s1 s1]);
-A = R*S;
-F = [A*y_1;A*y_2] - [y_gnd_1;y_gnd_2];
 end
 function F = distance3D(x,y,y_gnd)
 s1 = x(1);
@@ -59,12 +63,33 @@ Ry = [cos(beta) 0 sin(beta);0 1 0;-sin(beta) 0 cos(beta)];
 Rx = [1 0 0;0 cos(alpha) -sin(alpha);0 sin(alpha) cos(alpha)];
 R = Rz*Ry*Rx;
 S = diag([s1 s1 s1]);
-    y_est = R*S*y;
-    y_err = y_gnd - y_est;
-    y_err = y_err.^2;
-    F = sqrt(y_err(1,:)+y_err(2,:)+y_err(3,:));
+y_est = R*S*y;
+y_err = y_gnd - y_est;
+y_err = y_err.^2;
+F = sqrt(y_err(1,:)+y_err(2,:)+y_err(3,:));
 end
-
+function F = distanceTrans(x,y,y_gnd)
+s1 = x(1);
+alpha = x(2);
+beta = x(3);
+gamma = x(4);
+xTrans = x(5);
+yTrans = x(6);
+zTrans = x(7);
+T = [xTrans;yTrans;zTrans];
+Rz = [cos(gamma) -sin(gamma) 0;sin(gamma) cos(gamma) 0;0 0 1];
+Ry = [cos(beta) 0 sin(beta);0 1 0;-sin(beta) 0 cos(beta)];
+Rx = [1 0 0;0 cos(alpha) -sin(alpha);0 sin(alpha) cos(alpha)];
+R = Rz*Ry*Rx;
+S = diag([s1 s1 s1]);
+A = [S*R T;zeros(1,3) 1];
+y_aug = [y;ones(1,size(y,2))];
+y_est = A*y_aug;
+y_est = y_est(1:3,:)./y_est(4,:);
+y_err = y_gnd - y_est;
+y_err = y_err.^2;
+F = sqrt(y_err(1,:)+y_err(2,:)+y_err(3,:));
+end
 function [R_total, t_total] = icp_3d(p_t, p_s)
 k = 0;
 R_total = eye(3);
@@ -107,13 +132,6 @@ for iter = 1:100
     t_total = R*t_total+t;
     R_total = R*R_total;
 
-    %% Terminate when [R, t] is very close to [I, 0]
-%     delta = norm([R - eye(3), t]);
-%     if delta > sqrt(eps)
-%         k = k + 1;
-%     else
-%         break
-%     end
 end
 
 end
