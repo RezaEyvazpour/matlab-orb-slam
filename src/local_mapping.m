e6381707778ad1c5811e8679b5bb70225e9dc28f
snehchav <snehchav@umich.edu>
2018-04-11 14:28:59 -0400

    Keyframe Culling

--- src/local_mapping.m
+++ src/local_mapping.m
@@ -1,4 +1,4 @@
-function local_mapping(keyFrame)
+function local_mapping()
 
 global Map
 global State
@@ -29,6 +29,54 @@ global Debug
 %   For all keyframes
 %       Check if 90% of map points are present in at least 3 other key
 %       frames at the same or finer scale
+
+vs = Map.covisibilityGraph;
+i=1;
+check_map_point=[];
+check_match_map_point=[];
+while(1)
+    keyframe1=vs.Views.ViewId(i);
+    num_key_frames=vs.NumViews
+    [descriptors_all, points_all] = vs.getAllMapPoints(keyframe1);
+    map_points_keyframe1=points_all.Count;
+    check_map_point=[check_map_point; [map_points_keyframe1, keyframe1]];
+    count=0;
+    for j=i+1:num_key_frames
+        keyframe2=vs.Views.ViewId(j);
+        connections = vs.getAllConnections();
+        [conn_row, conn_col]=size(connections);
+        check_conn=repmat([keyframe1,keyframe2],conn_row,1);
+        Lia = ismember(connections,check_conn,'rows');
+        if(~nnz(Lia))
+            break;
+        end
+        [matches] = vs.getMatches(keyframe1, keyframe2);
+        [match_map_points,~]=size(matches);
+
+
+        check_match_map_point=[check_match_map_point; [match_map_points,keyframe1,keyframe2]];
+
+
+        if ((match_map_points/map_points_keyframe1)*100 >= 0.9*map_points_keyframe1)
+            count=count+1;
+        end
+        flag_del=0;
+        if(count==3)
+            vs = vs.deleteKeyFrame(keyframe1);
+            i=i;
+            flag_del=1;
+            break;
+        end
+    end
+    if (flag_del==0)
+        i=i+1;
+    end
+    if (i>vs.NumViews)
+        break;
+    end
+end
+
+Map.covisibilityGraph=vs;
 
 end
 
